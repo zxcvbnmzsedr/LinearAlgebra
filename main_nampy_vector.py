085fe10f1f7a9347b088b6ba159b1669e2a99f64
zxcvbnmzsedr <tianzneg@gmail.com>
2019-02-15 19:21:55 +0800

    三方库倒进来看看

---
+++ main_nampy_vector.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+if __name__ == '__main__':
+    print(np.__version__)
+
+    var = np.array([1, 2, 3])
+    print(var)
+
+    print(np.zeros(5))
+    print(np.full(5, 6))
+
+    print(var.size)
+
+    print(len(var))
+
